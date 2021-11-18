b850679d092ecbd9c6895cf7d77f705ff57ed5a7
rkrohne1991 <rafal.krohne@hotmail.co.uk>
2021-11-18 08:23:06 +0000

    Form Submissions In TypeScript Projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
+import NewTodo from "./components/NewTodo";
 
 function App() {
   const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
 
   return (
     <div>
+      <NewTodo />
       <Todos items={todos} />
     </div>
   );
